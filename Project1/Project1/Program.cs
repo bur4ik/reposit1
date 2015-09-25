81b68d4c21f0b344764b1993d79cc5314f09d332
bur4ik <bur4ik66@gmail.com>
2015-09-25 22:45:40 +0300

    0

--- Project1/Project1/Program.cs
+++ Project1/Project1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Project1
@@ -28,7 +29,22 @@ namespace Project1
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Drow();
             snake.Move();
-
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
+            Thread.Sleep(300);
+            snake.Move();
 
             Console.ReadLine();
         }
